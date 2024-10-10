69973c129fb1042c5d28cfcd979392b21813ad79
krishnadevchandhu <151444997+krishnadevchandhu@users.noreply.github.com>
2024-10-10 14:49:41 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,31 @@
+let navbar = document.querySelector('.header .navbar');
+
+
+
+document.querySelector('#nav-close').onclick = () =>{
+    navbar.classList.remove('active');
+}
+
+let searchForm = document.querySelector('.search-form');
+
+
+
+
+
+
+window.onscroll=()=>{
+    navbar.classList.remove('active');
+    if(window.scrollY>0){
+        document.querySelector('.header').classList.add('active');
+    }else{
+        document.querySelector('.header').classList.remove('active');
+    }
+};
+window.onload=()=>{
+    if(window.scrollY>0){
+        document.querySelector('.header').classList.add('active');
+    }else{
+        document.querySelector('.header').classList.remove('active');
+    }
+};
+
